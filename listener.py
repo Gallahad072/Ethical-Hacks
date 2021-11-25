09ac5f1b0371eb6d0454999c45d4df51e7e35671
Gallahad072 <gallahad072@gmail.com>
2021-11-25 15:13:10 +0000

    gets new device ip if outdated

--- listener.py
+++ listener.py
@@ -2,17 +2,13 @@ import subprocess
 import nmap
 from decouple import config
 
-IP_NETWORK = config("IP_NETWORK")
-IP_DEVICE = config("IP_PHONE")
-DEVICE_NAME = config("PHONE_NAME")
-
 
 # Gets list of hostnames and ips active on your network
 # Beware: very slow
-def getHosts():
+def getHosts(network_ip):
     hosts = {}
     nmScan = nmap.PortScanner()
-    nmScan.scan(IP_NETWORK)
+    nmScan.scan(network_ip)
     host_list = nmScan.all_hosts()
     for host in host_list:
         hosts[nmScan[host].hostname()] = host
@@ -20,25 +16,43 @@ def getHosts():
     return hosts
 
 
+def getDeviceIp(device_name):
+    IP_NETWORK = config("IP_NETWORK")
+    while True:
+        print("here")
+        hosts = getHosts(IP_NETWORK)
+        print(hosts)
+        device_ip = hosts.get(device_name)
+        print(device_ip)
+        if device_ip:
+            print("returned")
+            return device_ip
+
+
 # Listens whether a device is or isn't on the network and announces
 def listenForRhys():
-    process = subprocess.Popen(["ping", IP_DEVICE], stdout=subprocess.PIPE)
+    DEVICE_IP = config("IP_PHONE")
+    DEVICE_NAME = config("PHONE_NAME")
+    process = subprocess.Popen(["ping", DEVICE_IP], stdout=subprocess.PIPE)
     home = True
     while True:
         line = process.stdout.readline()
         connected_ip = line.decode("utf-8").split()[3]
-        if connected_ip == f"{IP_DEVICE}:" and not home:
+        if connected_ip == f"{DEVICE_IP}:" and not home:
             nmScan = nmap.PortScanner()
-            nmScan.scan(IP_DEVICE)
-            if nmScan[IP_DEVICE].hostname() == DEVICE_NAME:
+            nmScan.scan(DEVICE_IP)
+            if nmScan[DEVICE_IP].hostname() == DEVICE_NAME:
                 subprocess.Popen(["say", "Rhys is Home"])
                 home = True
+            else:
+                # Sets new device ip if device name not on active ip
+                DEVICE_IP = getDeviceIp(DEVICE_NAME)
         elif home:
             check = 0
             for i in range(10):
                 line = process.stdout.readline()
                 connected_ip = line.decode("utf-8").split()[3]
-                if connected_ip != f"{IP_DEVICE}:":
+                if connected_ip != f"{DEVICE_IP}:":
                     check += 1
             if check >= 8:
                 subprocess.Popen(["say", "Rhys is not Home"])
