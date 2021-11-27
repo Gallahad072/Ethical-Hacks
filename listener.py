48999ed131c22d09244eb8fcbbdac1e740ac8189
Gallahad072 <gallahad072@gmail.com>
2021-11-27 21:38:04 +0000

    Listener.py now listens to a name or an ip

--- listener.py
+++ listener.py
@@ -20,8 +20,8 @@ def getHosts():
     return hosts
 
 
-# Returns an active device name
-def getDeviceName():
+# Returns an active device name or ip
+def getDevice():
     print("Loading ...")
     hosts = getHosts()
     print("\nOptions:")
@@ -31,25 +31,28 @@ def getDeviceName():
         choice = input(f"\nChoose host to listen to (1/{len(hosts)}): ")
         if choice.isdigit():
             if 1 <= int(choice) <= len(hosts):
-                name = list(hosts.keys())[int(choice) - 1]
-                confirm = input(f"Listen to {name}? (y/n): ")
-                if confirm == "y":
-                    break
-    return name
+                name, ip = list(hosts.items())[int(choice) - 1]
+                break
+
+    return (
+        ip
+        if input(f"list for 1: Name({name}) or 2: IP({ip})? (1/2): ") == "2"
+        else name
+    )
 
 
 # Announces a message
-def announce(name, home):
+def announce(device, home):
     past_verb = "arrived" if home else "left"
     time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    message = f"{name} has {past_verb} at {time}"
+    message = f"{device} has {past_verb} at {time}"
     print(message)
 
 
 # Listens whether a device is or isn't on the network
-def listen(name):
+def listen(device):
     print("Listening...")
-    process = subprocess.Popen(["ping", name], stdout=subprocess.PIPE)
+    process = subprocess.Popen(["ping", device], stdout=subprocess.PIPE)
     home = False
     while True:
         line = process.stdout.readline()
@@ -64,18 +67,18 @@ def listen(name):
                     left = False
             if left:
                 home = False
-                announce(name, home)
+                announce(device, home)
 
         elif bytes == "64":
             home = True
-            announce(name, home)
+            announce(device, home)
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        listen(getDeviceName())
+        listen(getDevice())
     if len(sys.argv) == 2:
-        name = sys.argv[1]
-        listen(name)
+        device = sys.argv[1]
+        listen(device)
     else:
-        print("Usage: python listener.py <device_name>")
+        print("Usage: python listener.py <device name or ip>")
